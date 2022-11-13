8792df63dd4ad1a8e7a2add9f601889f9512e522
sechevarria98 <samechevarria2@gmail.com>
2022-11-12 19:15:15 -0600

    select the specified size from the drop down; click the add to cart button

--- Bot.js
+++ Bot.js
@@ -1,6 +1,25 @@
 const puppeteer = require("puppeteer");
+const URL = "https://www.nike.com/t/air-force-1-07-mens-shoes-5QFp5Z/CW2288-111";
+const specifiedSize = "10";
+
 
 (async () => {
-    const browser = await puppeteer.launch()
+    // setting headless to false for now
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+
+    await page.goto(URL);
+    await page.waitForSelector(".add-to-cart-form.nike-buying-tools");
+
+    await page.evaluate((specifiedSize) => {
+        const sizeDropDown = document.querySelectorAll(".add-to-cart-form.nike-buying-tools div fieldset div div input");
+        let specifiedIndex = Array.from(sizeDropDown)
+                .map((item, index) => (item.getAttribute("value").split(":")[1] === specifiedSize ? i : false))
+                .filter(Boolean)[0];
+        
+        sizeDropDown[specifiedIndex].click();
+    }, specifiedSize);
     
+    await page.click('button[class="ncss-btn-primary-dark btn-lg add-to-cart-btn"]');
+    await browser.close();
 })();
